930d287443dbb1589f0c207eda57033d6ed6d473
Scroph <xifer@bayn.ma>
2012-07-31 04:12:02 +0000

    Update includes/core_class.php
    Reduced the validate_post() method's code.

--- includes/core_class.php
+++ includes/core_class.php
@@ -5,33 +5,8 @@ class Core {
 	// Function to validate the post data
 	function validate_post($data)
 	{
-		// Counter variable
-		$counter = 0;
-
-		// Validate the hostname
-		if(isset($data['hostname']) AND !empty($data['hostname'])) {
-			$counter++;
-		}
-		// Validate the username
-		if(isset($data['username']) AND !empty($data['username'])) {
-			$counter++;
-		}
-		// Validate the password
-		if(isset($data['password']) AND !empty($data['password'])) {
-		  // pass
-		}
-		// Validate the database
-		if(isset($data['database']) AND !empty($data['database'])) {
-			$counter++;
-		}
-
-		// Check if all the required fields have been entered
-		if($counter == '3') {
-			return true;
-		}
-		else {
-			return false;
-		}
+		/* Validating the hostname, the database name and the username. The password is optional. */
+		return !empty($data['hostname']) && !empty($data['username']) && !empty($data['database']);
 	}
 
 	// Function to show an error
